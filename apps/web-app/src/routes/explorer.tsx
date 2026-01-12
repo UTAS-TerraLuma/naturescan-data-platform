164b3f121c4ebdfd4794120f48a108145224b6d4
James Gregory <james.gregory@utas.edu.au>
2026-01-12 16:47:32 +1100

    wip explorer with new stac

--- apps/web-app/src/routes/explorer.tsx
+++ apps/web-app/src/routes/explorer.tsx
@@ -1,4 +1,5 @@
 import { MapLayout } from "@/components/layouts/map-layout"
+import { ExplorerMap } from "@/components/map/explorer-map"
 import { ViewStateDebug } from "@/components/map/view-state-debug"
 import { createFileRoute } from "@tanstack/react-router"
 
@@ -8,8 +9,16 @@ export const Route = createFileRoute("/explorer")({
 
 function RouteComponent() {
     return (
-        <MapLayout header={<h1>Explorer</h1>} overlay={<ViewStateDebug />}>
-            Explorer
+        <MapLayout
+            header={<h1>Explorer</h1>}
+            overlay={
+                <>
+                    <div className="absolute top-2 left-2 w-xs space-y-2"></div>
+                    <ViewStateDebug />
+                </>
+            }
+        >
+            <ExplorerMap />
         </MapLayout>
     )
 }
